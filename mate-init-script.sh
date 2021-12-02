ce2f34ffe16911e666985057f0bdd708ade050ca
mt-chilgab <53109916+mt-chilgab@users.noreply.github.com>
2021-12-02 20:21:20 +0900

    Add files via upload

---
+++ mate-init-script.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+SCIPRTPATH=$(dirname "$(readlink -f "$0")")
+
+case "$1" in
+    restart)
+        sed -i "/^${SCRIPTPATH}/mate-init-script.sh restart/d" /home/${USER}/.bashrc
+
+        ;;
+    *)
+        echo -e "\n${SCRIPTPATH}/mate-init-script.sh restart" >> /home/${USER}/.bashrc
+        if [ $(ls /sys/class/net | grep wlan0) == "wlan0" ]; then
+            echo "SSID?" && read SSID
+            echo "PW?" && read PW
+            sudo echo -e "\n    wifis:\
+                          \n        wlan0:\
+                          \n            optional: true\
+                          \n            access-points:\
+                          \n                \"${SSID}\":\
+                          \n                        password: \"${PW}\"\
+                          \n            dhcp4: true" >> /etc/netplan/50-cloud-init.yaml
+            echo "Now rebooting..." && sleep 2s
+            sudo reboot    
+        else 
+            echo "No Wi-Fi class found... Please make sure your RPi support Wi-Fi connection..."
+        fi
+        ;;
+esac
